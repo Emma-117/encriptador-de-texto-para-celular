4ab9bf410e9e9e978b5f409e0604bb892619771d
Emma-117 <159651135+Emma-117@users.noreply.github.com>
2024-07-24 13:30:20 -0500

    Aqui subi mis archivos para el Challenge
    del Encriptador de texto para Alura Latam

---
+++ index.js
@@ -0,0 +1,47 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "https://github.com/Apuntes-De-Un-Ingeniero-Youtube/text-encriptor/blob/main/img/mu%C3%B1eco.png?raw=true";
+    } else {
+        muñeco.src = "https://github.com/Apuntes-De-Un-Ingeniero-Youtube/text-encriptor/blob/main/img/mu%C3%B1eco.png?raw=true";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+
+    let textoDesencriptado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoDesencriptado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";
+    } else {
+        muñeco.src = "./img/encriptado.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
